bac484c21213e9864dbca00291c2abdf2b2426ff
osdoonhyun <skw1996@naver.com>
2023-07-12 14:22:16 +0900

    remove: FindId, FindPassword 삭제

--- src/routers.js
+++ src/routers.js
@@ -1,6 +1,4 @@
 import { createBrowserRouter } from 'react-router-dom';
-import FindId from './pages/FindId';
-import FindPassword from './pages/FindPassword';
 import SingUp from './pages/Signup';
 import ChangePassword from './pages/ChangePassword';
 import Login from './pages/Login';
@@ -14,14 +12,6 @@ const router = createBrowserRouter([
   {
     path: '/login',
     element: <Login />,
-  },
-  {
-    path: '/findid',
-    element: <FindId />,
-  },
-  {
-    path: '/findpassword',
-    element: <FindPassword />,
   },
   {
     path: '/signup',
